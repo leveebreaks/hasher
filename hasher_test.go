aca91b48e3536a38f81c503a3f66ba54d15d3bef
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2021-11-01 19:09:53 +0200

    test added for the checkpasswordhash func

--- hasher_test.go
+++ hasher_test.go
@@ -17,7 +17,7 @@ func TestHashPassword(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			"TestHashPassword Positive",
+			"HashPassword Positive",
 			args{password: "testinput"},
 			"4LdZ8zau/S/1sxU08j2Yzt/cpAeFC6XGyZUCxCREGrc=",
 			false,
@@ -32,6 +32,31 @@ func TestHashPassword(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("HashPassword() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+	type args struct {
+		password string
+		hash     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"CheckPasswordHash equals",
+			args{"testinput", "4LdZ8zau/S/1sxU08j2Yzt/cpAeFC6XGyZUCxCREGrc="},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CheckPasswordHash(tt.args.password, tt.args.hash); got != tt.want {
+				t.Errorf("CheckPasswordHash() = %v, want %v", got, tt.want)
 			}
 		})
 	}
